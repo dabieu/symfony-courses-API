a7da74df190bdac0f108817cd0b55bb0def34126
Daniel Cardoso <daniel.cardoso@satellitus.com>
2022-01-31 17:56:39 -0300

    First commit: All CRUD operations for COURSE

---
+++ src/Controller/CourseController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Course;
+
+/**
+ * @Route("/courses", name="course_")
+ */
+class CourseController extends AbstractController
+{
+    /**
+     * @Route("/", name="index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $courses = $this->getDoctrine()->getRepository(Course::class)->findAll();
+
+        return $this->json([
+            'data' => $courses
+        ]);
+    }
+
+    /**
+     * @Route("/{courseId}", name="show", methods={"GET"})
+     */
+    public function show($courseId)
+    {
+        $course = $this->getDoctrine()->getRepository(Course::class)->find($courseId);
+
+        return $this->json([
+            'data' => $course
+        ]);
+
+    }
+
+     /**
+     * @Route("/", name="create", methods={"POST"})
+     */
+    public function create(Request $request)
+    {
+        $data = $request->request->all();
+
+        $course = new Course();
+        $course->setTitle($data['title']);
+        $course->setDescription($data['description']);
+        $course->setStartDate(\DateTime::createFromFormat('d-m-Y', $data['startdate']));
+        $course->setEndDate(\DateTime::createFromFormat('d-m-Y', $data['enddate']));
+
+        $doctrine = $this->getDoctrine()->getManager();
+
+        $doctrine->persist($course);
+        $doctrine->flush();
+
+        return $this->json([
+            'data' => 'Curso cadastrado com sucesso!'
+        ]);
+    }
+
+     /**
+     * @Route("/{courseId}", name="update", methods={"PUT", "PATCH"})
+     */
+    public function update($courseId, Request $request)
+    {
+        $data = $request->request->all();
+        $doctrine = $this->getDoctrine();
+
+        $course = $doctrine->getRepository(Course::class)->find($courseId);
+
+        if($request->request->has('title'))
+            $course->setTitle($data['title']);
+            
+        if($request->request->has('description'))
+            $course->setDescription($data['description']);
+
+        if($request->request->has('startdate'))    
+            $course->setStartDate(\DateTime::createFromFormat('d-m-Y', $data['startdate']));
+
+        if($request->request->has('enddate'))
+            $course->setEndDate(\DateTime::createFromFormat('d-m-Y', $data['enddate']));
+
+        $manager = $doctrine->getManager();
+        $manager->flush();
+
+        return $this->json([
+            'data' => 'Curso atualizado com sucesso!'
+        ]);
+
+    }
+
+    /**
+     * @Route("/{courseId}", name="delete", methods={"DELETE"})
+     */
+    public function delete($courseId)
+    {
+        $doctrine = $this->getDoctrine();
+        $course = $doctrine->getRepository(Course::class)->find($courseId);
+
+        $manager = $doctrine->getManager();
+        $manager->remove($course);
+        $manager->flush();
+
+        return $this->json([
+            'data' => 'Curso removido com sucesso!'
+        ]);
+    }
+}
---
+++ src/Entity/Course.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CourseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CourseRepository::class)
+ */
+class Course
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $startDate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $endDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTimeInterface $startDate): self
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTimeInterface $endDate): self
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+}
